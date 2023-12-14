3de2ee1ad8dd9f1d7522ee454f253966d9870705
VitaliyGerasimov <gerasimov-1996@inbox.ru>
2023-12-14 15:43:48 +0500

    Second change

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,28 +1,23 @@
 package web.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import web.Dao.CarDao;
 import web.model.Car;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
-    private List<Car> cars;
+    private final CarDao carDao;
 
-    public CarServiceImpl() {
-        cars = new ArrayList<>();
-        cars.add(new Car("Toyota", "Black", 200));
-        cars.add(new Car("Honda", "White", 350));
-        cars.add(new Car("Ford", "Black", 126));
-        cars.add(new Car("Chevrolet", "Red", 210));
-        cars.add(new Car("BMW", "Orange", 320));
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
     }
 
     @Override
     public List<Car> getCars(int count) {
-
+        List<Car> cars = carDao.getCars();
         if (count >= 5) {
             return cars;
         } else {
